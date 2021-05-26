7cc80a0bc010f5dced39d946d34568b4dadd3e66
trungnt1991 <84711569+trungnt1991@users.noreply.github.com>
2021-05-26 10:20:46 +0700

    new

---
+++ na/admin/add-product.php
@@ -0,0 +1,100 @@
+<?php
+require_once('form-product.php');
+
+$categoryList = executeResult('select * from category');
+$id = getGet('id');
+if ($id > 0) {
+	$thisProduct = executeResult('select * from product where id = '.$id, true);
+}else {
+	$thisProduct = null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Product - Page</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+	<!-- include summernote css/js -->
+	<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h2 class="text-center">Trang chỉnh sửa</h2>
+			</div>
+			<div class="panel-body">
+				<form method="post">
+					<div class="form-group">
+						<label for="title">Tên sản phẩm:</label>
+						<input required="true" type="text" class="form-control" id="title" name="title" value="<?=($thisProduct != null)?$thisProduct['title']:''?>">
+						<input type="text" name="id" value="<?=($thisProduct != null)?$thisProduct['id']:''?>" style="display: none;">
+					</div>
+					<div class="form-group">
+						<label for="thumbnail">Hình Ảnh:</label>
+						<input required="true" type="text" class="form-control" id="thumbnail" name="thumbnail" value="<?=($thisProduct != null)?$thisProduct['thumbnail']:''?>">
+					</div>
+					<div class="form-group">
+						<label for="price">Giá:</label>
+						<input required="true" min="0" type="number" class="form-control" id="price" name="price" value="<?=($thisProduct != null)?$thisProduct['price']:''?>">
+					</div>
+					<div class="form-group">
+						<label for="category_id">Danh mục sản phẩm:</label>
+						<select required="true" class="form-control" id="category_id" name="category_id">
+							<option value="">-- Chọn danh mục --</option>
+							<?php
+								foreach ($categoryList as $item) {
+									if($thisProduct != null && $item['id'] == $thisProduct['category_id']) {
+										echo '<option selected value="'.$item['id'].'">'.$item['title'].'</option>';
+									} else {
+										echo '<option value="'.$item['id'].'">'.$item['title'].'</option>';
+									}
+								}
+							?>
+					  </select>
+					</div>
+					<div class="form-group">
+						<label for="content">Content:</label>
+						<textarea class="form-control" id="content" name="content"<?=($thisProduct != null)?$thisProduct['content']:''?>></textarea>
+					</div>
+					<a href="edit-product.php"><button class="btn btn-success">Lưu</button></a>
+				</form>
+			</div>
+		</div>
+	</div>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+	   $('#content').summernote({
+        height: 200,
+        toolbar: [
+            ['style', ['style']],
+            ['font', ['bold', 'italic', 'underline', 'clear']],
+            ['fontname', ['fontname']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture', 'video']],
+            ['misc', ['fullscreen', 'codeview', 'undo', 'redo', 'help']]
+          ]
+      });
+	});
+</script>
+</body>
+</html>
---
+++ na/db/config.php
@@ -0,0 +1,7 @@
+<?php
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'newbie');
+
+define('MD5_PRIMARY_KEY', 'fass');
