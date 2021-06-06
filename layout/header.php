bb8eaa37abfc243c0e84bf3ef0a32166b49624e2
trungnt1991 <trung.nt.1022@aptechlearning.edu.vn>
2021-06-06 10:09:34 +0700

    header

--- layout/header.php
+++ layout/header.php
@@ -1,89 +1,190 @@
 <?php
 require_once '../db/dbhelper.php';
-  $categoryList = executeResult("select * from category");
-  // var_dump($categoryList);
+	$categoryList = executeResult("select * from category");
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-  <!-- Popper JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
-  <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-  <!-- Font Awesome 4.7 CDN -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Font Awesome 4.7 CDN -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <!-- css -->
-  <link rel="stylesheet" type="text/css" href="../layout/style.css">
+	
+<style>
+
+.navbar {
+
+	/*position: fixed;*/
+ 	top: 20;
+}
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+.topnav .icon {
+  display: none;
+}
+
+.dropdown {
+  float: left;
+  overflow: hidden;
+}
+
+.dropdown .dropbtn {
+  font-size: 17px;    
+  border: none;
+  outline: none;
+  color: white;
+  padding: 14px 16px;
+  background-color: inherit;
+  font-family: inherit;
+  margin: 0;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  float: none;
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.topnav a:hover, .dropdown:hover .dropbtn {
+  background-color: #555;
+  color: white;
+}
+
+.dropdown-content a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav a:not(:first-child), .dropdown .dropbtn {
+    display: none;
+  }
+  .topnav a.icon {
+    float: right;
+    display: block;
+  }
+}
+
+@media screen and (max-width: 600px) {
+  .topnav.responsive {position: relative;}
+  .topnav.responsive .icon {
+    position: absolute;
+    right: 0;
+    top: 0;
+  }
+  .topnav.responsive a {
+    float: none;
+    display: block;
+    text-align: left;
+  }
+  .topnav.responsive .dropdown {float: none;}
+  .topnav.responsive .dropdown-content {position: relative;}
+  .topnav.responsive .dropdown .dropbtn {
+    display: block;
+    width: 100%;
+    text-align: left;
+  }
+}
+</style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg heading">
-  <a class="navbar-brand" href="../home/trangchu.php">
-    <img src="https://static.wixstatic.com/media/4e382d_0d579ec3ec6241af9e1380ed79c56b7b~mv2.png/v1/fill/w_406,h_46,al_c,q_85,usm_0.66_1.00_0.01/Asset%202_3x.webp" alt="logo" style="width:100%;">
-  </a>
-  <ul>
-    <li>
-      <a href="../home/trangchu.php">trang chủ</a>
-    </li>
-    <li>
-      <a href="../home/about.php">giới thiệu</a>
-    </li>
-    <li class="dropdown">
-        <a class="dropdown-toggle" href="../admin/list-product.php" data-toggle="dropdown">sản phẩm</a>
-            <div class="dropdown-menu" style="border: none; font-weight: bold;">
-              <!-- list do ra tu database-->
-              <ol style="list-style-type: none;width: 300px;">
-            <?php
-                    foreach ($categoryList as $item) {
-                      echo '<li>
-                        <a href="../admin/list-product.php?id='.$item['id'].'">'.$item['title'].'</a>';
-                      echo "</li>";
-                    }
-                    ?>
-                </ol>
-          </div>
-      </li>
-    <li>
-      <a href="../page/gallery.php">thư viện ảnh</a>
-    </li>
-    <li>
-      <a href="../page/tips.php">tips chia sẻ</a>
-    </li>
-    <li>
-      <a href="../home/contact-us.php">liên hệ</a>
-    </li>
-    <li>
-      <a href="../user/login.php"><i class="fa fa-user" aria-hidden="true"></i> đăng nhập</a>
-    </li>
-  </ul>
+
+<div class="topnav navbar" id="myTopnav">
+	<a class="navbar-brand" href="../home/trangchu.php">
+		<img src="https://static.wixstatic.com/media/4e382d_0d579ec3ec6241af9e1380ed79c56b7b~mv2.png/v1/fill/w_406,h_46,al_c,q_85,usm_0.66_1.00_0.01/Asset%202_3x.webp" alt="logo" style="width:100%;">
+	</a>
+    <a href="../home/trangchu.php">trang chủ</a>
+	<a href="../home/about.php">giới thiệu</a>
+	<div class="dropdown">
+		<button class="dropbtn" href="../admin/list-product.php">sản phẩm 
+			<i class="fa fa-caret-down"></i>
+		</button>
+	    <div class="dropdown-content">
+	    	<?php
+		        foreach ($categoryList as $item) {
+		          echo '<a href="../admin/list-product.php?id='.$item['id'].'">'.$item['title'].'</a>';
+		        }
+	        ?>
+		</div>
+	</div>      
+	<a href="../page/gallery.php">thư viện ảnh</a>
+	<a href="../page/tips.php">tips chia sẻ</a>
+	<a href="../home/contact-us.php">liên hệ</a>
+	<a href="../user/login.php"><i class="fa fa-user" aria-hidden="true"></i> đăng nhập</a>
+	<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 <?php
-  $cart = [];
-  if(isset($_COOKIE['cart'])) {
-    $json = $_COOKIE['cart'];
-    $cart = json_decode($json, true);
-  }
-  $count = 0;
-  foreach ($cart as $item) {
-    $count += $item['num'];
-  }
+	$cart = [];
+	if(isset($_COOKIE['cart'])) {
+		$json = $_COOKIE['cart'];
+		$cart = json_decode($json, true);
+	}
+	$count = 0;
+		foreach ($cart as $item) {
+		$count += $item['num'];
+	}
 ?>
-    <a href="../cart/cart.php">
-      <button type="button" class="btn btn-outline-danger btn-sm" style="font-size: 23px;border: none;margin-left:50px;">
-        <i class="fa fa-shopping-cart" aria-hidden="true"></i> Giỏ hàng <span style="font-weight: bold; color: black;"><?=$count?></span>
-      </button>
-    </a>
-  </div>
-</nav>
+	<a href="../cart/cart.php">
+		<button type="button" class="btn btn-outline-danger btn-sm" style="font-size: 23px;border: none;margin-left:50px;">
+			<i class="fa fa-shopping-cart" aria-hidden="true"></i> Giỏ hàng <span style="font-weight: bold; color: black;"><?=$count?></span>
+		</button>
+	</a>
+</div>
+
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script>
